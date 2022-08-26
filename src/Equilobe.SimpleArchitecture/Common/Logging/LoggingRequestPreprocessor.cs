6d54759ced1fa96e041a2aacaf42eb59f8696dbd
SEBASTIANDUBA11\sebastiandumitrascu <sebastian.dumitrascu@equilobe.com>
2022-08-26 23:58:21 +0300

    - fixed null warnings in LoggingRequestPreprocessor - fixed warnings in GetList featuer

--- src/Equilobe.SimpleArchitecture/Common/Logging/LoggingRequestPreprocessor.cs
+++ src/Equilobe.SimpleArchitecture/Common/Logging/LoggingRequestPreprocessor.cs
@@ -1,48 +1,44 @@
 ﻿using MediatR;
 using MediatR.Pipeline;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace Equilobe.SimpleArchitecture.Common.Logging
+namespace Equilobe.SimpleArchitecture.Common.Logging;
+
+public class LoggingRequestPreprocessor<TRequest> : IRequestPreProcessor<TRequest>
+    where TRequest : notnull
 {
-    public class LoggingRequestPreprocessor<TRequest> : IRequestPreProcessor<TRequest>
+    private static readonly JsonSerializerSettings LoggingJsonSettings;
+
+    private readonly ILogger _logger;
+
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    static LoggingRequestPreprocessor()
     {
-        private static readonly JsonSerializerSettings LoggingJsonSettings;
-
-        private readonly ILogger _logger;
-
-        private readonly IHttpContextAccessor _httpContextAccessor;
-
-        static LoggingRequestPreprocessor()
+        LoggingJsonSettings = new JsonSerializerSettings
         {
-            LoggingJsonSettings = new JsonSerializerSettings
-            {
-                // using a custome resolver to ignore properties during logging
-                // [JsonIgnore] is not used since it affects model binding
-                ContractResolver = new LoggingJsonResolver(),
-                NullValueHandling = NullValueHandling.Ignore,
-                Formatting = Formatting.None
-            };
-        }
+            // using a custome resolver to ignore properties during logging
+            // [JsonIgnore] is not used since it affects model binding
+            ContractResolver = new LoggingJsonResolver(),
+            NullValueHandling = NullValueHandling.Ignore,
+            Formatting = Formatting.None
+        };
+    }
 
-        public LoggingRequestPreprocessor(ILogger<TRequest> logger, IHttpContextAccessor httpContextAccessor)
-        {
-            _logger = logger;
-            _httpContextAccessor = httpContextAccessor;
-        }
+    public LoggingRequestPreprocessor(ILogger<TRequest> logger, IHttpContextAccessor httpContextAccessor)
+    {
+        _logger = logger;
+        _httpContextAccessor = httpContextAccessor;
+    }
 
-        public Task Process(TRequest request, CancellationToken cancellationToken)
-        {
-            var currentUser = _httpContextAccessor.HttpContext?.User?.Identity?.Name ?? "Not Present";
-            var type = request.GetType().FullName.Replace("+", ".");
-            var json = JsonConvert.SerializeObject(request, LoggingJsonSettings);
+    public Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        var currentUser = _httpContextAccessor.HttpContext?.User?.Identity?.Name ?? "Not Present";
+        var type = request.GetType().FullName?.Replace("+", ".");
+        var json = JsonConvert.SerializeObject(request, LoggingJsonSettings);
 
-            _logger.LogInformation($"Handling a request: Type={type}; Request={json}; User={currentUser}");
+        _logger.LogInformation($"Handling a request: Type={type}; Request={json}; User={currentUser}");
 
-            return Unit.Task;
-        }
+        return Unit.Task;
     }
 }
